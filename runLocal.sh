07012a9f00250728dd173d5202639a3c701415a8
rf <rf@ryans-Air.lexicondigital.com.au>
2023-10-18 11:25:44 +1100

    bash script for local deployment

---
+++ runLocal.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+minikube start
+
+kubectl create -f kubernetes/observability-namespace.yml
+kubectl create -f kubernetes/cert-manager.yml
+
+webhookReady=false
+checkCount=0
+
+while [ $webhookReady == false ]
+do
+  checkCount=$(($checkCount + 1))
+  echo "checking cert manager webhook -> $checkCount times"
+  sleep 3
+  wh=$(kubectl get deployment cert-manager-webhook -n cert-manager -o jsonpath="{.status.readyReplicas}")
+
+  if [ ! -z $wh ]
+  then
+    webhookReady=true
+  fi
+done
+
+echo "----------------------------------"
+echo "cert manager webhook ready"
+echo "----------------------------------"
+
+kubectl create -f kubernetes/jaeger-operator.yml
+
+operator=false
+checkCount=0
+
+while [ $operator == false ]
+do
+  checkCount=$(($checkCount + 1))
+  echo "checking jaeger operator -> $checkCount times"
+  sleep 3
+  wh=$(kubectl get deployment jaeger-operator -n observability -o jsonpath="{.status.readyReplicas}")
+
+  if [ ! -z $wh ]
+  then
+    operator=true
+  fi
+done
+
+echo "----------------------------------"
+echo "jaeger operator ready"
+echo "----------------------------------"
+
+tilt up
+
