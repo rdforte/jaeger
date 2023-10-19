23f7c7dd1bc8d532d142fc92d62a5182e09312fb
rf <rf@ryans-MacBook-Air.local>
2023-10-19 18:28:37 +1100

    temp restart

--- runLocal.sh
+++ runLocal.sh
@@ -8,20 +8,13 @@ kubectl create -f kubernetes/cert-manager.yml
 kubectl create -f kubernetes/elastic-crds.yml
 kubectl create -f kubernetes/elastic-operator.yml
 
-webhookReady=false
 checkCount=0
 
-while [ $webhookReady == false ]
+while [ -z $(kubectl get deployment cert-manager-webhook -n cert-manager -o jsonpath="{.status.readyReplicas}") ]
 do
   checkCount=$(($checkCount + 1))
   echo "checking cert manager webhook -> $checkCount times"
   sleep 3
-  wh=$(kubectl get deployment cert-manager-webhook -n cert-manager -o jsonpath="{.status.readyReplicas}")
-
-  if [ ! -z $wh ]
-  then
-    webhookReady=true
-  fi
 done
 
 echo "----------------------------------"
@@ -30,20 +23,15 @@ echo "----------------------------------"
 
 kubectl create -f kubernetes/jaeger-operator.yml
 
-operator=false
 checkCount=0
 
-while [ $operator == false ]
+while [ -z $(kubectl get deployment jaeger-operator -n observability -o jsonpath="{.status.readyReplicas}") ]
 do
   checkCount=$(($checkCount + 1))
   echo "checking jaeger operator -> $checkCount times"
   sleep 3
-  wh=$(kubectl get deployment jaeger-operator -n observability -o jsonpath="{.status.readyReplicas}")
 
-  if [ ! -z $wh ]
-  then
-    operator=true
-  fi
+  restart_run_local $checkCount
 done
 
 echo "----------------------------------"
@@ -53,7 +41,39 @@ echo "----------------------------------"
 kubectl apply -f kubernetes/elasticsearch.yml
 kubectl create secret generic jaeger-secret --from-literal=ES_USERNAME=elastic --from-literal=ES_PASSWORD=$(kubectl get secret quickstart-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
 
-#kubectl apply -f kubernetes/jaeger-tracing.yml
+es="Pending"
+checkCount=0
+
+while [ $es != "Running" ]
+do
+  checkCount=$(($checkCount + 1))
+  echo "checking elastic search running -> $checkCount times"
+  sleep 3
+  es=$( kubectl get pod quickstart-es-default-0 -o jsonpath="{.status.phase}")
+done
+
+echo "----------------------------------"
+echo "elasticsearch ready"
+echo "----------------------------------"
+
+kubectl apply -f kubernetes/jaeger-tracing.yml
 
 tilt up
+
+# HELPERS
+
+function restart_run_local() {
+  retryCount=5
+  if [ -z $1 ]
+  then
+    return
+  fi
+
+  if [ $1 -ge $retryCount]
+  then
+    make stop
+    exit 1
+    make start
+  fi
+}
 
