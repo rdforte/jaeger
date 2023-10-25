adcd3a50ad6caaa146214cb01bdd01bb164d5505
rf <rf@ryans-MacBook-Air.local>
2023-10-25 11:52:39 +1100

    removed open telem

--- cmd/app/bar/main.go
+++ cmd/app/bar/main.go
@@ -1,91 +1,35 @@
 package main
 
 import (
-	"Tracing/internal/telemetry"
-	"context"
-	"errors"
 	"fmt"
-	"go.opentelemetry.io/otel"
 	"html"
 	"io"
 	"log"
-	"net"
 	"net/http"
-	"os"
-	"os/signal"
 	"time"
-
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
-var tracer = otel.Tracer("barHandler")
-
 func main() {
-	if err := run(); err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func run() (err error) {
-	// Handle SIGINT (CTRL+C) gracefully.
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	// Set up OpenTelemetry.
-	serviceName := "barService"
-	serviceVersion := "0.1.0"
-	otelShutdown, err := telemetry.SetupOTelSDK(ctx, serviceName, serviceVersion)
-	if err != nil {
-		return
-	}
-	// Handle shutdown properly so nothing leaks.
-	defer func() {
-		err = errors.Join(err, otelShutdown(context.Background()))
-	}()
-
 	// Start HTTP server.
 	srv := &http.Server{
 		Addr:         ":8080",
-		BaseContext:  func(_ net.Listener) context.Context { return ctx },
 		ReadTimeout:  time.Second,
 		WriteTimeout: 10 * time.Second,
 		Handler:      newHTTPHandler(),
 	}
-	srvErr := make(chan error, 1)
-	go func() {
-		srvErr <- srv.ListenAndServe()
-	}()
 
-	// Wait for interruption.
-	select {
-	case err = <-srvErr:
-		// Error when starting HTTP server.
-		return
-	case <-ctx.Done():
-		// Wait for first CTRL+C.
-		// Stop receiving signal notifications as soon as possible.
-		stop()
-	}
-
-	// When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
-	err = srv.Shutdown(context.Background())
-	return
+	log.Fatal(srv.ListenAndServe())
 }
 
 func newHTTPHandler() http.Handler {
 	mux := http.NewServeMux()
-
-	// handleFunc is a replacement for mux.HandleFunc
-	// which enriches the handler's HTTP instrumentation with the pattern as the http.route.
-	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-		// Configure the "http.route" for the HTTP instrumentation.
-		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
-		mux.Handle(pattern, handler)
-	}
-
-	// Register handlers.
-	handleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("http://foo:8080/foo")
+	mux.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		req, err := http.NewRequestWithContext(r.Context(), http.MethodGet, "http://foo:8080/foo", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		client := http.DefaultClient
+		resp, err := client.Do(req)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -100,7 +44,5 @@ func newHTTPHandler() http.Handler {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	// Add HTTP instrumentation for the whole server.
-	handler := otelhttp.NewHandler(mux, "/")
-	return handler
+	return mux
 }
